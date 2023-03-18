f166a5e7727d8e3eb2842c9206b11d8f83fd94cc
Viktor <viktor.hidirov@yandex.ru>
2023-03-18 18:50:17 +0700

    feat: авторизация пользователя

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './Header';
-import AppContainer from './AppContainer';
+import Diary from './Diary';
+import Tips from './Tips';
+import Register from './Register';
+import Login from './Login';
+import NavBar from './NavBar';
+import './styles/App.css';
+import ProtectedRoute from './ProtectedRoute'
 
-function App() {
-  return (
-    <>
-      <Header />
-      <AppContainer />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <main className="content">
+          {this.state.loggedIn && <NavBar />}
+          <Routes>
+
+
+            <Route path="/" element={this.state.loggedIn ? <Navigate to="/diary" replace /> : <Navigate to="/login" replace />} />
+            <Route path="/diary" element={<ProtectedRoute element={<Diary />} loggedIn={this.state.loggedIn} />} />
+            <Route path="/tips" element={<ProtectedRoute element={<Tips />} loggedIn={this.state.loggedIn} />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
